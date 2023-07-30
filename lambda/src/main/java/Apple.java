4642659f208d7a5615eb3bb536ff6aa23ef75ce9
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-07-30 00:41:43 -0600

    examples added behavioral parameterization.

---
+++ lambda/src/main/java/Apple.java
@@ -0,0 +1,10 @@
+public class Apple {
+
+    private String color;
+    private int weight;
+
+    public Apple(String color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+}
--- lambda/src/main/java/ArraySorting.java
+++ lambda/src/main/java/ArraySorting.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArraySorting {
 
+    public static void main(String[] args) {
 
+
+
+        ArraySorting as = new ArraySorting();
+//        as.sort();
+
+        QuickSort quickSort = new QuickSort();
+        BubbleSort bubbleSort = new BubbleSort();
+
+        as.sort(quickSort);
+        as.sort(bubbleSort);
+
+        List<Apple> appleList = new ArrayList<>();
+        appleList.add(new Apple("Green", 200));
+        appleList.add(new Apple("Red", 100));
+        appleList.add(new Apple("Green", 400));
+        appleList.add(new Apple("Red", 100));
+
+
+    }
+
+    private void sort(Sorting sorting) {
+        sorting.sort();
+    }
 }
