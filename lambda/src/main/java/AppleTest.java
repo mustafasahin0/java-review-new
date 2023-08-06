162229a141134d770917bb7f90d315cd490542a8
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-08-06 12:18:45 -0600

    added Predicate Functional Interface example

--- lambda/src/main/java/AppleTest.java
+++ lambda/src/main/java/AppleTest.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class AppleTest {
 
@@ -11,17 +12,15 @@ public class AppleTest {
         inventory.add(new Apple(200, Color.GREEN));
         inventory.add(new Apple(50, Color.RED));
 
-        List<Apple> heavyApple = filterApples(inventory, new AppleHeavyPredicate());
+        List<Apple> heavyApple = filterApples(inventory, apple -> apple.getWeight() > 200);
         System.out.println(heavyApple);
 
-        List<Apple> greenApple = filterApples(inventory, new AppleGreenColorPredicate());
+        List<Apple> greenApple = filterApples(inventory, apple -> apple.getColor().equals(Color.GREEN));
         System.out.println(greenApple);
     }
 
-    private static List<Apple> filterApples(List<Apple> inventory, ApplePredicate applePredicate) {
-
+    private static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> applePredicate) {
         List<Apple> result = new ArrayList<>();
-
         for (Apple apple : inventory) {
             if (applePredicate.test(apple)) {
                 result.add(apple);
