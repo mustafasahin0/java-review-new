57780aa51a9b4987836000777bb76482984a5dfb
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-08-06 11:16:00 -0600

    added Ready Functional Interfaces

--- lambda/src/main/java/functionalinterfaces/Examples.java
+++ lambda/src/main/java/functionalinterfaces/Examples.java
@@ -1,6 +1,6 @@
 package functionalinterfaces;
 
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public interface Examples {
 
@@ -8,17 +8,31 @@ public interface Examples {
 
         //******************PREDICATE******************
 
-//        Predicate<Integer> lessThan = new Predicate<Integer>() {
-//            @Override
-//            public boolean test(Integer integer) {
-//                return integer < 18;
-//            }
-//        };
-//
-//        System.out.println(lessThan.test(10));
-
         Predicate<Integer> lessThan = a -> a < 18; // Implementation of test method that belongs to Predicate interface
         System.out.println(lessThan.test(50));
+
+        //******************CONSUMER******************
+
+        Consumer<Integer> display = i -> System.out.println(i);
+        display.accept(5);
+
+        //******************BICONSUMER******************
+
+        BiConsumer<Integer, Integer> addTwo = (x, y) -> System.out.println(x + y);
+        addTwo.accept(100, 200);
+
+        //******************FUNCTION******************
+
+        Function<String, String> fun = s -> "Hello " + s;
+        String str = fun.apply("Ozzy");
+        System.out.println(str);
+
+        System.out.println(fun.apply(("Apple")));
+
+        //******************SUPPLIER******************
+
+        Supplier<Double> randomValue = () -> Math.random();
+        System.out.println(randomValue.get());
 
     }
 }
