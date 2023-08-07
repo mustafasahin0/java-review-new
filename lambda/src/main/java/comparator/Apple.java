9933f7b69f11c242ba27383ef8bd55b03b205217
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-08-06 19:33:34 -0600

    compareTo method added

---
+++ lambda/src/main/java/comparator/Apple.java
@@ -0,0 +1,11 @@
+package comparator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Apple {
+    private int weight;
+    private Color color;
+}
--- lambda/src/main/java/comparator/ComparatorDemo.java
+++ lambda/src/main/java/comparator/ComparatorDemo.java
@@ -1,9 +1,8 @@
 package comparator;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
+import static java.util.Comparator.comparing;
 
 public class ComparatorDemo {
 
@@ -30,6 +29,23 @@ public class ComparatorDemo {
         list.sort(((o1, o2) -> o2.compareTo(o1)));
         System.out.println(list);
 
+        List<Apple> myInventory = Arrays.asList(
+                new Apple(80, Color.GREEN),
+                new Apple(155, Color.RED),
+                new Apple(120, Color.GREEN)
+        );
 
+        Comparator<Apple> sortApple = comparing((apple) -> apple.getWeight());
+        myInventory.sort(sortApple);
+        System.out.println(myInventory);
+
+        myInventory.sort(comparing(Apple::getWeight));
+
+        //Reversed
+        myInventory.sort(comparing(Apple::getWeight).reversed());
+
+        //Chaining
+        myInventory.sort(comparing(Apple::getColor).reversed().thenComparing(Apple::getColor));
+        System.out.println(myInventory);
     }
 }
