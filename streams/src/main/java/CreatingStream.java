8802de735c4a7586a730a5d48e32bb4cee3b9b53
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-08-13 21:00:10 -0600

    Stream added

--- streams/src/main/java/CreatingStream.java
+++ streams/src/main/java/CreatingStream.java
@@ -1,2 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class CreatingStream {
+
+    public static void main(String[] args) {
+
+        // 1. Creating Stream from Array ------------------
+        String[] courses = {"Java", "JS", "TS"};
+        Stream<String> courseStream = Arrays.stream(courses);
+
+        // 2. Creating Stream from Collection ---------------
+        List<String> courseList = Arrays.asList("Java", "DS", "Spring");
+        Stream<String> courseStream2 = courseList.stream();
+
+        List<Course> myCourses = Arrays.asList(
+                new Course("Java", 100),
+                new Course("DS", 101),
+                new Course("Spring", 102),
+                new Course("Microservices", 103)
+        );
+
+        Stream<Course> myCourseStream = myCourses.stream();
+
+        // 3. Creating Stream from Values ------------------
+        Stream<Integer> stream = Stream.of(1,2,3,4);
+
+    }
 }
